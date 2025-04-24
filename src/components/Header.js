f1177aa5a2f98d698303e0e76c2a55a4cd71b776
Erick Mora <77127047+moraole@users.noreply.github.com>
2025-04-24 11:28:18 -0700

    Update Header.js Revert changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import styled, { css } from 'styled-components';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import StarsBackground from './StarsBackground';
 import lightTheme from '../themes/lightTheme';
 import darkTheme from '../themes/darkTheme';
@@ -8,6 +8,82 @@ import Project2 from './Project2';
 import Project3 from './Project3';
 import Project4 from './Project4';
 import SmallDeviceBox from './logoBox';
+import { isVisible } from '@testing-library/user-event/dist/utils';
+
+const CenteredLink = styled.a`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme === lightTheme ? 'black' : 'white'};
+  text-decoration: none;
+  font-weight: bold;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const RadialGradient = styled.div`
+  cursor: none;
+  position: absolute;
+  ${({ isVisible, projectHovered, projectMenuHovered }) => isVisible && !projectHovered && !projectMenuHovered ? 'width: 0px;' : 'width: 20px'};
+  ${({ isVisible, projectHovered, projectMenuHovered }) => isVisible && !projectHovered && !projectMenuHovered ? 'height: 0px;: -22px' : 'height: 20px'};
+  mix-blend-mode: difference;
+  background: radial-gradient(
+    ellipse at center,
+    ${({ isDarkMode }) => isDarkMode ? 'transparent 0%,' : 'white 100%,'}
+    ${({ isDarkMode }) => isDarkMode ? 'transparent 0%,' : 'white 100%,'}
+    ${({ projectHovered, isDarkMode }) =>
+    projectHovered ? 'brown 300%' : isDarkMode ? 'white 40%' : 'black 40%'} ,
+    ${({ projectHovered, isDarkMode }) =>
+    projectHovered ? 'brown 100%' : isDarkMode ? 'white 100%' : 'black 100%'} 
+  );
+  clip-path: ${({ projectMenuHovered, projectHovered, isVisible, isDarkMode }) =>
+    projectMenuHovered || projectHovered || !isDarkMode
+      ? 'circle(10px at center)'
+      : 'polygon(50% 0%, 61.8% 38.2%, 100% 50%, 61.8% 61.8%, 50% 100%, 38.2% 61.8%, 0% 50%, 38.2% 38.2%)'};
+  
+  ${({ isVisible, projectHovered, projectMenuHovered }) => isVisible && !projectHovered && !projectMenuHovered ? 'clip-path: none' : ''};
+  pointer-events: none;
+  z-index: 9000;
+  animation: glowing 1s infinite alternate;
+  transition: transform 0.3s ease;
+  
+  transform: ${({ projectMenuHovered }) => (projectMenuHovered ? 'scale(1.0)' : 'scale(2.0)')};
+  
+  ${({ projectHovered }) => projectHovered ? `transform: scale(5.0);` : ``};
+  @keyframes glowing {
+    0% {
+      opacity: 0.7;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+    &:before {
+    content: '${({ isVisible, projectHovered, projectMenuHovered }) => (!isVisible || projectHovered || projectMenuHovered ? '' : 'click to close menu')}';
+    position: absolute;
+    font-size: 9px;
+    width: 70px;
+    height: 10px;
+    border: dotted 0.2px;
+    border-top: 0px; 
+    border-bottom: 0px; 
+    padding: 5px;
+    color: white; // Change the color to match your design
+    cursor: pointer;
+    mix-blend-mode: difference;
+    top: 50%; // Adjust the positioning as needed
+    left: 50%; // Adjust the positioning as needed
+    transform: translate(-50%, -50%);
+    @
+  }
+  @media (max-width: 900px) {
+    display:none;
+}
+`;
+
 
 const Moon = styled.div`
   position: absolute;
@@ -19,29 +95,11 @@ const Moon = styled.div`
   z-index: 10;
   top: 2.5vh;
   right: 10vw;
-
+  
   @media (max-width: 900px) {
     height: 50px;
     width: 50px;
-  }
-`;
-
-const LinkedInButton = styled.a`
-  position: absolute;
-  bottom: 20px;
-  left: 20px;
-  padding: 10px 20px;
-  font-size: 1rem;
-  font-weight: bold;
-  color: white;
-  background-color: #0077b5; /* LinkedIn blue */
-  text-decoration: none;
-  border-radius: 5px;
-  transition: background-color 0.3s;
-
-  &:hover {
-    background-color: #005582;
-  }
+}
 `;
 
 const Logo = styled.div`
@@ -60,13 +118,19 @@ const Logo = styled.div`
   top: 0;
   border-radius: 8px;
   padding: 20px;
+`;
 
-  @media (max-width: 900px) {
-    font-size: 4rem;
-    width: 100%;
-    height: auto;
-    padding: 10px;
-  }
+const HeaderContent = styled.div`
+  overflow: hidden;
+  transition: background-color 0.3s, color 0.3s;
+  background: ${({ theme }) =>
+    theme === lightTheme ? 'white' : '#181e2a'};
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
 `;
 
 const ExpandableLetter = styled.span`
@@ -77,6 +141,7 @@ const ExpandableLetter = styled.span`
     font-size: 40px;
     opacity: 1;
     transform: translate(40px, -55px);
+
   }
 `;
 
@@ -93,6 +158,7 @@ const ExpandedText = styled.span`
   opacity: 0;
   transition: opacity 0.3s ease, transform 0.3s ease;
   pointer-events: none;
+
 `;
 
 const ProjectMenu = styled.div`
@@ -106,10 +172,11 @@ const ProjectMenu = styled.div`
   padding: 10px;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: flex-start; // Align project items to the left
 
   @media (max-width: 900px) {
     background-color: ${({ theme }) => theme.phonebg};
+    
     top: 50%;
     right: 0%;
     left: 2%;
@@ -119,23 +186,26 @@ const ProjectMenu = styled.div`
   }
 `;
 
+const WorkHeading = styled.div`
+  font-family: 'DM Mono', monospace;
+  display: absolute;
+  
+  font-size: 16px;
+  margin-bottom: 10px;
+`;
+
 const ProjectMenuItem = styled.div`
   font-family: 'DM Mono', monospace;
   display: flex;
   align-items: center;
-  width: 100%;
+  width: 340px;
   margin: 5px 0;
   cursor: pointer;
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: ${({ theme }) => theme === lightTheme ? '#f0f0f0' : '#333'};
-  }
 `;
 
 const ProjectNumber = styled.div`
   margin-left: 20px;
-  width: 30px;
+  width: 30px; // Adjust the width as needed
   text-align: left;
 `;
 
@@ -145,74 +215,150 @@ const ProjectName = styled.div`
 `;
 
 const ProjectYear = styled.div`
-  width: 50px;
+  width: 50px; // Adjust the width as needed
   text-align: right;
+
 `;
+
+
 
 const HeaderContainer = styled.div`
   position: relative;
   overflow: hidden;
 `;
-
-const HeaderContent = styled.div`
-  overflow: hidden;
-  transition: background-color 0.3s, color 0.3s;
-  background: ${({ theme }) =>
-    theme === lightTheme ? 'white' : '#181e2a'};
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  width: 100%;
-`;
-
 const ProjectContainer = styled.div`
-  transition: max-height 0.5s ease, opacity 0.5s ease;
-  max-height: ${({ isVisible }) => (isVisible ? '1000px' : '0')};
-  opacity: ${({ isVisible }) => (isVisible ? '1' : '0')};
-  overflow: hidden;
-`;
 
-const Header = ({ isDarkMode }) => {
+`
+
+const Header = ({ isDarkMode, isThemeButtonHovered }) => {
   const [hoveredE1, setHoveredE1] = useState(false);
   const [hoveredE2, setHoveredE2] = useState(false);
   const [hoveredM, setHoveredM] = useState(false);
   const [hoveredO, setHoveredO] = useState(false);
+  const [hovering, setHovering] = useState(false);
+  const [cursorPos, setCursorPos] = useState({ x: 0, y: 0 });
   const [projectMenuHovered, setProjectMenuHovered] = useState(false);
   const [selectedProject, setSelectedProject] = useState('');
+  const [isVisible, setIsVisible] = useState(false);
+  const [projectHovered, setProjectHovered] = useState(false);
+  const [projectClosed, setProjectClosed] = useState(false);
   const isPhone = window.innerWidth <= 900;
 
-  const handleProjectItemClick = (projectName) => {
-    if (selectedProject !== projectName) {
-      setSelectedProject(projectName);
-    } else {
-      setSelectedProject('');
-    }
+  const handleMouseEnter = () => {
+    setHovering(true);
   };
 
-  return (
-    <HeaderContainer>
-      <HeaderContent>
-        {isDarkMode ? <Moon /> : null}
+  const handleMouseLeave = () => {
+    setHovering(false);
+  };
 
-        <ProjectMenu
+  const handleMouseMove = (e) => {
+    setCursorPos({ x: e.clientX, y: e.clientY });
+  };
+
+  const handleProjectItemClick = (projectName) => {
+    if (selectedProject != projectName) {
+        if (selectedProject) {
+          setTimeout(() => {
+            setSelectedProject(projectName);
+            setIsVisible(true);
+          }, 300);
+        }
+        else {
+          setSelectedProject(projectName);
+          setIsVisible(true);
+        }
+      }
+      else {
+        setTimeout(() => {
+          setIsVisible(false);
+          setSelectedProject('');
+        }, 300);
+      }
+  };
+
+  const handleCloseProject = () => {
+    if (!projectMenuHovered && !projectHovered) {
+      setTimeout(() => {
+        setSelectedProject('');
+        setProjectClosed(true);
+        setIsVisible(false);
+      }, 300);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener('mouseenter', handleMouseEnter);
+    document.addEventListener('mouseleave', handleMouseLeave);
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mouseenter', handleMouseEnter);
+      document.removeEventListener('mouseleave', handleMouseLeave);
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+  // Inside your Header.js
+  useEffect(() => {
+    document.addEventListener('mouseenter', handleMouseEnter);
+    document.addEventListener('mouseleave', handleMouseLeave);
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mouseenter', handleMouseEnter);
+      document.removeEventListener('mouseleave', handleMouseLeave);
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+  return (
+    <HeaderContainer
+      onClick={handleCloseProject}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onMouseMove={handleMouseMove}
+      hovering={hovering}
+    >
+      {hovering && (
+        <RadialGradient
+          isVisible={isVisible}
+          isDarkMode={isDarkMode}
+          projectMenuHovered={projectMenuHovered}
+          projectHovered={projectHovered}
+          cursorPos={cursorPos}
+          style={{
+            top: `${cursorPos.y - 10}px`,
+            left: `${cursorPos.x - 10}px`,
+          }}
+        />
+      )}
+        <HeaderContent>
+          {isDarkMode ? (
+            <>
+              <Moon />
+            </>
+          ) : (
+            <>
+
+            </>
+          )}
+
+            <ProjectMenu
           onMouseEnter={() => setProjectMenuHovered(true)}
           onMouseLeave={() => setProjectMenuHovered(false)}
         >
-          <ProjectMenuItem onClick={() => handleProjectItemClick('project1')}>
+          <WorkHeading>Work:</WorkHeading>
+          <ProjectMenuItem onClick={() => handleProjectItemClick('project3')}>
             <ProjectNumber>01</ProjectNumber>
             <ProjectName>WorldCup Database using PostgresSQL</ProjectName>
             <ProjectYear>2023</ProjectYear>
           </ProjectMenuItem>
-          <ProjectMenuItem onClick={() => handleProjectItemClick('project2')}>
+          <ProjectMenuItem onClick={() => handleProjectItemClick('project1')}>
             <ProjectNumber>02</ProjectNumber>
             <ProjectName>SITE 2023 Publication</ProjectName>
             <ProjectYear>2023</ProjectYear>
           </ProjectMenuItem>
-          <ProjectMenuItem onClick={() => handleProjectItemClick('project3')}>
+          <ProjectMenuItem onClick={() => handleProjectItemClick('project2')}>
             <ProjectNumber>03</ProjectNumber>
-            <ProjectName>League Of Legends Player Comparison App</ProjectName>
+            <ProjectName>League Of Legends Player Comparison App </ProjectName>
             <ProjectYear>2025</ProjectYear>
           </ProjectMenuItem>
           <ProjectMenuItem onClick={() => handleProjectItemClick('project4')}>
@@ -221,14 +367,22 @@ const Header = ({ isDarkMode }) => {
             <ProjectYear>2024</ProjectYear>
           </ProjectMenuItem>
         </ProjectMenu>
+          <StarsBackground isDarkMode={isDarkMode} />
+          {isPhone ? (
+            
+            isDarkMode ?
+              <SmallDeviceBox isDarkMode={isDarkMode} />
+              :
+              <SmallDeviceBox isDarkMode={isDarkMode} />
+              
 
-        <StarsBackground isDarkMode={isDarkMode} />
-
-        {isPhone ? (
-          <SmallDeviceBox isDarkMode={isDarkMode} />
-        ) : (
-          <Logo>
+              
+            
+          ) : (
+            <>
+            <Logo>
             <ExpandableLetter
+              isPhone={isPhone}
               onMouseEnter={() => setHoveredE1(true)}
               onMouseLeave={() => setHoveredE1(false)}
             >
@@ -259,31 +413,54 @@ const Header = ({ isDarkMode }) => {
               <ExpandedText style={{ opacity: hoveredO ? 1 : 0 }}>lmedo</ExpandedText>
             </ExpandableLetter>
           </Logo>
-        )}
-      </HeaderContent>
-
-      <ProjectContainer isVisible={selectedProject === 'project1'}>
-        <Project1 isDarkMode={isDarkMode} />
-      </ProjectContainer>
-      <ProjectContainer isVisible={selectedProject === 'project2'}>
-        <Project2 isDarkMode={isDarkMode} />
-      </ProjectContainer>
-      <ProjectContainer isVisible={selectedProject === 'project3'}>
-        <Project3 isDarkMode={isDarkMode} />
-      </ProjectContainer>
-      <ProjectContainer isVisible={selectedProject === 'project4'}>
-        <Project4 isDarkMode={isDarkMode} />
-      </ProjectContainer>
-
-      {/* LinkedIn Button */}
-      <LinkedInButton
-        href="https://www.linkedin.com/in/your-linkedin-profile"
-        target="_blank"
-        rel="noopener noreferrer"
+            </>
+          ) }
+          
+        </HeaderContent>
+      <ProjectContainer
+        onMouseEnter={() => setProjectHovered(true)}
+        onMouseLeave={() => setProjectHovered(false)}
       >
-        Connect on LinkedIn
-      </LinkedInButton>
+        {selectedProject == 'project1' ?
+          <Project1 
+            isDarkMode={isDarkMode}
+            isVisible={false}
+            isThemeButtonHovered={isThemeButtonHovered}
+          /> 
+          : 
+          null
+          }
+        {selectedProject == 'project2' ?
+          <Project2
+            isDarkMode={isDarkMode}
+            isVisible={false}
+            isThemeButtonHovered={isThemeButtonHovered}
+          />
+          :
+          null
+        }
+        {selectedProject == 'project3' ?
+          <Project3
+            isDarkMode={isDarkMode}
+            isVisible={false}
+            isThemeButtonHovered={isThemeButtonHovered}
+          />
+          :
+          null
+        }
+        {selectedProject == 'project4' ?
+          <Project4
+            isDarkMode={isDarkMode}
+            isVisible={false}
+            isThemeButtonHovered={isThemeButtonHovered}
+          />
+          :
+          null
+        }
+      </ProjectContainer>
+
     </HeaderContainer>
+
   );
 };
 
